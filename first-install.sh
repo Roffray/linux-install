81400adca94d2f950cab92c6023ad9fa3d2c2474
Geoffray <gv@vuble.tv>
2018-10-15 14:27:12 +0200

    update install script

--- first-install.sh
+++ first-install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo apt-get install git
 
 sudo apt-get install curl
@@ -6,5 +7,5 @@ sudo apt-get install guake
 sudo apt-get install tmux
 sudo apt-get install zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
+chsh -s $(which zsh)
 
